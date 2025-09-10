9704c3467e6845db46592fc055ac24fc044f17f3
mokib08 <mokib08@gmail.com>
2025-09-10 07:01:16 +0600

    learning

---
+++ Backend/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const authRoutes = require('./routes/index.routes')
+
+const app = express()
+app.use(express.json())
+
+app.use('/auth', authRoutes)
+
+
+module.exports = app
+
